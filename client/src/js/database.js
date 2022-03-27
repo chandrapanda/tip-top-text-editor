2df7d9d026bcb62feb8fd3ea04f4092eae9d6a9a
Chandra Holt <chandra_holt@hotmail.com>
2022-03-27 16:16:33 -0700

    Webpack build compiles without errors. TODO: Installation code

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -11,11 +11,39 @@ const initdb = async () =>
       console.log("jate database created");
     },
   });
+//SEE UNIT 19 DAY 3 ACTIVITY 23 FOR THIS LOGIC:
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error("putDb not implemented");
+export const putDb = async (content) => {
+  // Create a connection to the database database and version we want to use.
+  const jateDb = await openDB("jate", 1);
+
+  // Create a new transaction and specify the database and data privileges.
+  const tx = jateDb.transaction("jate", "readwrite");
+
+  // Open up the desired object store.
+  const store = tx.objectStore("jate");
+
+  // Use the .add() method on the store and pass in the content.
+  const request = store.add({ jate: content });
+
+  // Get confirmation of the request.
+  const result = await request;
+  //Log or error out
+  console.log("🚀 - data saved to the database", result);
+  console.error("putDb not implemented");
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error("getDb not implemented");
+export const getDb = async () => {
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readonly");
+  const store = tx.objectStore("jate");
+  const request = store.getAll();
+  const result = await request;
+  console.log("result.value", result);
+  console.error("getDb not implemented");
+  return result;
+};
 
 initdb();
